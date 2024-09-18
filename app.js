0e7358b62539c469f4744d6479045c1a95af3342
ChatthongPP <chatthong.pp@gmail.com>
2024-09-19 02:16:14 +0700

    update code

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
